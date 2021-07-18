249c2668f18d6d6cf434e80d1109798989bd6b08
Paul Newling - iMac <PaulNewling@Gmail.com>
2021-07-18 13:48:57 -0700

    file is able to be passed to the back end and then converted to wasm file

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const fileUpload = require('express-fileupload');
 
 const { v4: uuidv4 } = require('uuid');
 const fs = require('fs');
+const fsPromises = require('fs').promises;
 const util = require('util');
 const path = require('path');
 const exec = require('child_process').exec;
@@ -26,66 +27,59 @@ app.get('/', (req, res) => {
 
 app.post('/data', async (req, res) => {
   //   console.log(req.body.filetype);
-  console.log(req.files);
+  //   console.log(req.files);
+  Promise.resolve(convertFile(req)).then(()=>{
 
+  });
+});
+
+async function convertFile(req) {
   const workingFolder = uuidv4();
- console.log(path.join(__dirname + '/tempData', workingFolder));
-  fs.mkdir(
-      path.join(__dirname + '/tempData', workingFolder),
-      (err) => {
-        if (err) {
-          return console.error(err);
-        }
-        console.log('Directory created successfully!');
 
-        console.log(workingFolder);
-        const filePath = path.join(__dirname, `/tempData/${workingFolder}/`);
-        fs.writeFile(filePath+req.files.file.name, req.files.file.data, function (err) {
-          if (err) return console.log(err);
-          console.log('Created File');
-        });
-      }
-    )
+  const fileName = req.files.file.name;
+  const fileData = req.files.file.data;
 
 
-  /*   fs.mkdir(((__dirname + '/testDir'), workingFolder), (err) => {
+  // Create a folder in the tempData folder
+  fs.mkdir(path.join(__dirname + '/tempData', workingFolder), (err) => {
     if (err) {
       return console.error(err);
     }
-    const fileloc = `__dirname/testDir/${workingFolder}/${req.files.file.name}`;
 
-    fs.writeFile(fileloc, req.files.file.data, function (err) {
-      if (err) return console.log(err);
-      console.log('Created File');
-    });
     console.log('Directory created successfully!');
-  }); */
+    console.log(workingFolder);
 
-  /* tmp.dir(function _tempDirCreated(err, __dirname, cleanupCallback) {
-    if (err) throw err;
-   
-    console.log('Dir: ', __dirname);
-    
-    // Manual cleanup
-    cleanupCallback();
-}); */
+    // Set file path to the new folder and then write the file
+    const filePath = path.join(__dirname, `/tempData/${workingFolder}/`);
+    fsPromises
+      .writeFile(filePath + fileName, fileData, (err) => {
+        if (err) return console.log(err);
+        console.log('Created File');
+      })
+      .then(() => {
+        const cmdLine = `emcc ${fileName} -s STANDALONE_WASM -o output.wasm`;
 
-  /* temp.track();
-temp.mkdir(path.join(__dirname , 'testDir'), (err) => {
-    if (err) {
-        return console.error(err);
-    }
-    console.log('Directory created successfully!');
-}); */
-
-  /* fs.mkdir(path.join(__dirname + '/tempData', 'test'), (err) => {
-    if (err) {
-        return console.error(err);
-    }
-    console.log('Directory created successfully!');
-});
- */
-});
+        // Run the command line in the folder that was created
+        exec(
+          cmdLine,
+          {
+            cwd: path.join(__dirname, `/tempData/${workingFolder}/`),
+          },
+          (error, stdout, stderr) => {
+            if (error) {
+              console.log(`error: ${error.message}`);
+              return;
+            }
+            if (stderr) {
+              console.log(`stderr: ${stderr}`);
+              return;
+            }
+            console.log(`stdout: ${stdout}`);
+          }
+        );
+      });
+  });
+}
 
 // Listen to the App Engine-specified port, or 8080 otherwise
 const PORT = process.env.PORT || 8080;
