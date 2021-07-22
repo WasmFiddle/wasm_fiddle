196bf42ba0ec1eeef07b4a0a05ec662fe5933d0d
Dan Allen <allend2@oregonstate.edu>
2021-07-22 13:45:09 -0700

    commit to pull frontend-backend-merge

--- public/js/script.js
+++ public/js/script.js
@@ -14,10 +14,11 @@ function embedFiddle(){
 }
 
 function goButton(){
-    let goBtn = document.getElementById("go-btn");
-    goBtn.addEventListener('click', ()=>{
-        serveFile();
-    });
+    // let goBtn = document.getElementById("go-btn");
+    // goBtn.addEventListener('click', ()=>{
+        // serveFile();
+    // });
+    // makeWasmFile('../../squarer.wasm');
 }
 
 function serveFile(){
@@ -38,3 +39,30 @@ function serveFile(){
     download.click();
     document.body.removeChild(download);
 }
+
+async function makeWasmFile(wasmFile){
+    await fetch(wasmFile)
+        .then(response => response.arrayBuffer())
+        .then(buffer => WebAssembly.compile(buffer))
+        .then(module => {
+            const instance = new WebAssembly.Instance(module);
+            console.log(instance.exports.squarer());
+        });
+}
+
+makeWasmFile('squarer.wasm');
+
+var squarer = () => {};
+
+// function loadWebAssembly(fileName) {
+//   return fetch(fileName)
+//     .then(response => response.arrayBuffer())
+//     .then(buffer => WebAssembly.compile(buffer))
+//     .then(module => {return new WebAssembly.Instance(module) });
+// };
+  
+// loadWebAssembly('squarer.wasm')
+//   .then(instance => {
+//     wasmBin = instance.exports._Z7squareri;
+//     console.log('Finished compiling! Ready when you are...');
+//   }); 
