466fb7b8f527c4cc97c9cba4bc584f9dc943a521
Paul Newling - iMac <PaulNewling@Gmail.com>
2021-07-24 12:36:58 -0700

    still gettting errors on the client side when trying to run WebAssembly

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ const fsPromises = require('fs').promises;
 const util = require('util');
 const path = require('path');
 const exec = require('child_process').exec;
+const execSync = require('child_process').execSync;
 
 app.use(
   fileUpload({
@@ -28,10 +29,84 @@ app.get('/', (req, res) => {
 app.post('/data', async (req, res) => {
   //   console.log(req.body.filetype);
   //   console.log(req.files);
-  Promise.resolve(convertFile(req)).then(()=>{
+  //   convertFile(req);
+  const workingFolder = uuidv4();
 
-  });
+  const fileName = req.files.file.name;
+  const fileData = req.files.file.data;
+
+  // Create a folder in the tempData folder
+  fsPromises
+    .mkdir(path.join(__dirname + '/tempData', workingFolder), (err) => {
+      if (err) {
+        return console.error(err);
+      }
+
+      console.log('Directory created successfully!');
+      console.log(workingFolder);
+    })
+    .then(() => {
+      // Set file path to the new folder and then write the file
+      const filePath = path.join(__dirname, `/tempData/${workingFolder}/`);
+      fsPromises
+        .writeFile(filePath + fileName, fileData, (err) => {
+          if (err) return console.log(err);
+          console.log('Created File');
+        })
+        .then(() => {
+          const cmdLine = `emcc ${fileName} -s EXPORTED_FUNCTIONS=_main -s STANDALONE_WASM -o output.wasm`;
+
+          // Run the command line in the folder that was created
+          execSync(
+            cmdLine,
+            {
+              cwd: path.join(__dirname, `/tempData/${workingFolder}/`),
+            },
+            (error, stdout, stderr) => {
+              if (error) {
+                console.log(`error: ${error.message}`);
+                return;
+              }
+              if (stderr) {
+                console.log(`stderr: ${stderr}`);
+                return;
+              }
+              console.log(`stdout: ${stdout}`);
+            }
+          );
+        })
+        .then(() => {
+          var options = {
+            root: filePath,
+          };
+          res.sendFile('output.wasm', options, function (err) {
+            if (err) {
+              console.log(err);
+            } else {
+              console.log('Sent File');
+            }
+          });
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    })
+    .catch((e) => {
+      console.log(e);
+    });
 });
+
+function execShellCommand(cmd) {
+  const exec = require('child_process').exec;
+  return new Promise((resolve, reject) => {
+    exec(cmd, (error, stdout, stderr) => {
+      if (error) {
+        console.warn(error);
+      }
+      resolve(stdout ? stdout : stderr);
+    });
+  });
+}
 
 async function convertFile(req) {
   const workingFolder = uuidv4();
@@ -39,46 +114,53 @@ async function convertFile(req) {
   const fileName = req.files.file.name;
   const fileData = req.files.file.data;
 
-
   // Create a folder in the tempData folder
-  fs.mkdir(path.join(__dirname + '/tempData', workingFolder), (err) => {
-    if (err) {
-      return console.error(err);
-    }
+  fsPromises
+    .mkdir(path.join(__dirname + '/tempData', workingFolder), (err) => {
+      if (err) {
+        return console.error(err);
+      }
 
-    console.log('Directory created successfully!');
-    console.log(workingFolder);
+      console.log('Directory created successfully!');
+      console.log(workingFolder);
+    })
+    .then(() => {
+      // Set file path to the new folder and then write the file
+      const filePath = path.join(__dirname, `/tempData/${workingFolder}/`);
+      fsPromises
+        .writeFile(filePath + fileName, fileData, (err) => {
+          if (err) return console.log(err);
+          console.log('Created File');
+        })
+        .then(() => {
+          const cmdLine = `emcc ${fileName} -o output.wasm`;
 
-    // Set file path to the new folder and then write the file
-    const filePath = path.join(__dirname, `/tempData/${workingFolder}/`);
-    fsPromises
-      .writeFile(filePath + fileName, fileData, (err) => {
-        if (err) return console.log(err);
-        console.log('Created File');
-      })
-      .then(() => {
-        const cmdLine = `emcc ${fileName} -s STANDALONE_WASM -o output.wasm`;
-
-        // Run the command line in the folder that was created
-        exec(
-          cmdLine,
-          {
-            cwd: path.join(__dirname, `/tempData/${workingFolder}/`),
-          },
-          (error, stdout, stderr) => {
-            if (error) {
-              console.log(`error: ${error.message}`);
-              return;
+          // Run the command line in the folder that was created
+          exec(
+            cmdLine,
+            {
+              cwd: path.join(__dirname, `/tempData/${workingFolder}/`),
+            },
+            (error, stdout, stderr) => {
+              if (error) {
+                console.log(`error: ${error.message}`);
+                return;
+              }
+              if (stderr) {
+                console.log(`stderr: ${stderr}`);
+                return;
+              }
+              console.log(`stdout: ${stdout}`);
             }
-            if (stderr) {
-              console.log(`stderr: ${stderr}`);
-              return;
-            }
-            console.log(`stdout: ${stdout}`);
-          }
-        );
-      });
-  });
+          );
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    })
+    .catch((e) => {
+      console.log(e);
+    });
 }
 
 // Listen to the App Engine-specified port, or 8080 otherwise
--- public/js/script.js
+++ public/js/script.js
@@ -16,7 +16,8 @@ function embedFiddle() {
 function goButton() {
   let goBtn = document.getElementById('go-btn');
   goBtn.addEventListener('click', () => {
-    sendFile();
+    //sendFile();
+    sendRunSource();
   });
 }
 
@@ -52,16 +53,98 @@ function sendFile() {
     type: 'text/plain',
   });
 
-  console.log(sourceFile);
+  //console.log(sourceFile);
 ;
   var fileData = new FormData();
   fileData.append('file', sourceFile);
   fileData.append('filetype', `${fileType}`);
   
+  var importObject = {
+    wasi_snapshot_preview1: {
+      'args_sizes_get': ()=>{},
+      'args_get': ()=>{},
+      'proc_exit': ()=>{},
+      'fd_write': ()=>{}
+    }, 
+    env: { 
+      'main': ()=>{}, 
+    }, 
+  }
 
   //This will need to be modified for production
   fetch('http://localhost:8080/data', {
     method: 'POST',
     body: fileData
-  });
+  }).then(response =>
+    response.arrayBuffer()
+  ).then(bytes =>
+    WebAssembly.instantiate(bytes, importObject)
+  ).then(result =>{
+    console.log(result.instance.exports);
+    result.instance.exports.exported_func();
+  }).catch(err =>{console.log(err)});
 }
+
+function sendRunSource() {
+    let fileData= packageSource()
+    
+      //This will need to be modified for production
+      fetch('/data', {
+          method: 'POST',
+          body: fileData
+      }).then(res=>{
+          return res.blob();
+      }).then(blob=>{
+          console.log(blob)
+          runWasm(blob)
+      }).catch(err=>console.log(err));
+  }
+  
+  async function runWasm(wasmFile){
+    // needed to instantiate sent file
+    let wasmMemory = new WebAssembly.Memory({initial: 2});
+    let importObject = {
+      wasi_snapshot_preview1: {
+        'args_sizes_get': ()=>{},
+        'args_get': ()=>{},
+        'proc_exit': ()=>{},
+        'fd_write': ()=>{}
+      }, 
+      env: { 
+        'main': ()=>{}, 
+      }, 
+      imports: {
+        memory: wasmMemory
+      }
+    }
+  
+    let buffer = await wasmFile.arrayBuffer(); 
+    WebAssembly.compile(buffer)
+      .then(module => WebAssembly.instantiate(module, importObject))
+      .then(wasmInstance => {
+        console.log(wasmInstance.exports);
+        // const aBuffer = wasmMemory.buffer;
+        // const newBuffer = new Uint8Array(aBuffer);
+        // console.log(newBuffer)
+        writeOutput(wasmInstance.exports.main());
+      })
+  }
+
+  function packageSource(){
+    const sourceText = document.getElementById('editing').value;
+    let fileType;
+    document.getElementsByName('options').forEach((element) => {
+      if (element.checked) fileType = element.value == 'rust' ? 'rs' : 'cpp';
+    });
+  
+    var sourceFile = new File([sourceText], `main.${fileType}`, {
+      type: 'text/plain',
+    });
+    // console.log(sourceFile);
+  
+    var fileData = new FormData();
+    fileData.append('file', sourceFile);
+    fileData.append('filetype', `${fileType}`);
+  
+    return fileData;
+  }
